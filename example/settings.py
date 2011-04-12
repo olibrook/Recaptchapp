d73120e4bdb068f6292d28cf03fc29715fee8dba
adam <adam@ebfb07ef-748b-4192-a4fe-89c62f4b7e82>
2011-04-12 10:12:06 +0000

    Porting latest changes to recaptchapp from Engage back into Djatato.

---
+++ example/settings.py
@@ -0,0 +1,10 @@
+
+
+RECAPTCHA_PUBLIC_KEY = "ABC"
+RECAPTCHA_PRIVATE_KEY = "XYZ"
+RECAPTCHA_USE_SSL = True #This gets set from request.is_secure() when you use the {% recaptcha_options %} template tag
+RECAPTCHA_OPTIONS = {
+    'theme' : 'custom',
+    'custom_theme_widget': 'recaptcha_widget',
+    #etc
+}
